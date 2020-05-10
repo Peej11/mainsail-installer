5ff66730ee11123f3e91429103d0157685630022
ArmyAg08 <2296277+ArmyAg08@users.noreply.github.com>
2020-05-10 10:10:38 -0500

    fix typo

--- mainsail_install.sh
+++ mainsail_install.sh
@@ -199,7 +199,7 @@ get_timezone_response() {
 }
 
 get_mcu_response() {
-  MCU_SETUP_RESPONSE=$(whiptail --title "Select MCU" --menu "Which MCU do you need to prepare Klipper for? This will selection will run make menuconfig in the background." 16 70 7 \
+  MCU_SETUP_RESPONSE=$(whiptail --title "Select MCU" --menu "Which MCU do you need to prepare Klipper for? This selection will run make menuconfig in the background." 16 70 7 \
       "SKR" "Bigtreetech SKR 1.3, SKR 1.4, SKR Mini E3" \
       "RAMPS" "RAMPs 1.4 or variant" \
       "Duet" "Duet or Duet Wifi" \
